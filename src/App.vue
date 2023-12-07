a758b38b4d2cec1d753aa2603f2ff3f84f0df511
KakioJ <1589610753@qq.com>
2023-12-07 09:59:44 +0800

    进击的巨人左侧栏初步绘制

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <landingPage msg="进击的巨人登录页面"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+import landingPage from './components/landingPage.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    landingPage
   }
 }
 </script>
--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -10,10 +10,52 @@
                 <img src="../assets/image/left/time.png" alt="进击的巨人时间标题">
                 <img src="../assets/image/left/text.png" alt="进击的巨人文字介绍">
             </div>
-            <div class="nav-">
-                
+            <div class="nav-list">
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/"><span>Facebook</span></a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/"><span>Twitter</span></a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/"><span>Instagram</span></a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/"><span>Youtube</span></a>
+                    </li>
+                </ul>
             </div>
         </div>
-        <div class="right-content"></div>
+        <div class="right-content">
+
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'landingPage',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url(../assets/css/attackontitan.css);
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+/* a {
+  color: #42b983;
+} */</style>
